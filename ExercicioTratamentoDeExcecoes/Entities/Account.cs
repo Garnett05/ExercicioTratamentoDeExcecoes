b2ec4eac6e55d4b9604bf01a4ca0bfad2410a9a2
Garnett05 <charlleslimamota10@hotmail.com>
2020-02-18 01:01:38 -0300

    Exercício tratando exceções padrões e exceções personalizadas para a solução

---
+++ ExercicioTratamentoDeExcecoes/Entities/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ExercicioTratamentoDeExcecoes.Entities.Exceptions;
+
+namespace ExercicioTratamentoDeExcecoes.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+        public void Withdraw(double amount)
+        {
+            if (Balance <= 0.0)
+            {
+                throw new DomainException ("WARNING: Not enought balance for the withdraw");
+            }
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("WARNING: The amount especify exceeds withdraw limit!");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("WARNING: The amount especify is higher then the account balance");
+            }
+            Balance -= amount;
+        }
+    }
+}
---
+++ ExercicioTratamentoDeExcecoes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using ExercicioTratamentoDeExcecoes.Entities;
+using ExercicioTratamentoDeExcecoes.Entities.Exceptions;
+
+namespace ExercicioTratamentoDeExcecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw Limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account acc = new Account(number, holder, balance, withdrawLimit);
+
+                Console.WriteLine();
+
+                Console.Write("Enter amount for withdraw: ");
+                double withdrawValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.Withdraw(withdrawValue);
+                Console.WriteLine("New balance: " + acc.Balance, CultureInfo.InvariantCulture);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error! " + e.Message);
+            }
+        }
+    }
+}
